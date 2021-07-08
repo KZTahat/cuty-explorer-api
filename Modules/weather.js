84e35c5ec4fdd01ea44234fffdd99bc4ecf2f420
KZTahat <kztahat96@gmail.com>
2021-07-08 12:50:46 +0300

    finishing

--- Modules/weather.js
+++ Modules/weather.js
@@ -2,27 +2,32 @@
 const axios = require("axios"); // for requsting APIs
 
 const utilities = {};
+let inMemory = {};
 
 // Weahter handler function
 utilities.weatherBitHandler = function (request, response) {
   let cityName = request.query.city_name;
   const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${process.env.WEATHER_API_KEY}&days=5`;
 
-  axios
-    .get(url)
-    .then((element) => {
-      console.log(element.data.data);
-      let weatherInfo = element.data.data.map((element2) => {
-        return new Forcast(element.data, element2);
+  if (inMemory[cityName] !== undefined) {
+    console.log("gteting from Memory");
+    response.send(inMemory[cityName]);
+  } else {
+    axios
+      .get(url)
+      .then((element) => {
+        console.log(element.data.data);
+        let weatherInfo = element.data.data.map((element2) => {
+          return new Forcast(element.data, element2);
+        });
+        inMemory[cityName] = weatherInfo;
+        console.log("hit the server");
+        response.send(weatherInfo);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
       });
-      // console.log(weatherInfo);
-      //     // weatherInfo.push(new Forcast(element.data));
-      console.log(weatherInfo);
-      response.send(weatherInfo);
-    })
-    .catch((error) => {
-      response.status(500).send(error);
-    });
+  }
 };
 
 class Forcast {
